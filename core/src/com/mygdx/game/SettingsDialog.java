99666474a343654ba18521723b8ac21dfb4f0b56
JustTeenager <56716766+JustTeenager@users.noreply.github.com>
2020-04-18 14:22:41 +0300

    Window Settings changing

--- core/src/com/mygdx/game/SettingsDialog.java
+++ core/src/com/mygdx/game/SettingsDialog.java
@@ -22,10 +22,13 @@ import javax.xml.soap.Text;
 public class SettingsDialog extends Actor {
     Window window;
     Texture backTxt;
+
     Texture line;
     private BitmapFont font;
+    private Stage st;
 
-    public SettingsDialog(int size, Stage st){
+    public SettingsDialog(float x, float y, float size, Stage st){
+        this.st=st;
         backTxt=new Texture("settingsPanel.png");
         line=new Texture("Shield Bar.png");
         font=new BitmapFont(Gdx.files.internal("liter.fnt"));
@@ -34,18 +37,40 @@ public class SettingsDialog extends Actor {
         Window.WindowStyle windowstyle=new Window.WindowStyle(font,new Color(0,0,0,0.55f),drawable);
         window=new Window("Settings",windowstyle);
         window.padTop(64);
-        window.setPosition(st.getWidth()/2-window.getWidth()/2,st.getHeight()/2-window.getHeight()/2);
-        window.setVisible(false);
+        window.setSize(600,400);
+        window.setPosition(x,y);
+        //window.setVisible(false);
+        this.setVisible(false);
         st.addActor(this);
     }
 
-    public void addButton(Button button){
+    public void addButton(Buttons button){
         this.window.add(button);
     }
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
-        //batch.draw();
         window.draw(batch, parentAlpha);
+        //batch.draw(line,st.getWidth()/2-window.getWidth()/2,st.getHeight()/2-window.getHeight()/2);
+    }
+
+    public Window getWindow() {
+        return window;
+    }
+
+    public Texture getBackTxt() {
+        return backTxt;
+    }
+
+    public Texture getLine() {
+        return line;
+    }
+
+    public BitmapFont getFont() {
+        return font;
+    }
+
+    public Stage getSt() {
+        return st;
     }
 }
